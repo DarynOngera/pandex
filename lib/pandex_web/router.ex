7db52ad200d83ecbe0e17ee749daec49c329feec
Ongera <ongeradaryn@gmail.com>
2026-05-03 02:01:02 +0300

    chore: fix root layout and update router

--- lib/pandex_web/router.ex
+++ lib/pandex_web/router.ex
@@ -6,6 +6,7 @@ defmodule PandexWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {PandexWeb.Layouts, :root}
+    plug :put_layout, false
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
@@ -20,8 +21,25 @@ defmodule PandexWeb.Router do
     get "/", PageController, :home
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", PandexWeb do
-  #   pipe_through :api
-  # end
+  scope "/", PandexWeb do
+    pipe_through :api
+
+    get "/.well-known/openid-configuration", OIDCController, :configuration
+    get "/.well-known/jwks.json", OIDCController, :jwks
+
+    post "/bootstrap/tenants", BootstrapController, :create_tenant
+    post "/bootstrap/users", BootstrapController, :create_user
+    post "/bootstrap/memberships", BootstrapController, :create_membership
+    post "/bootstrap/clients", BootstrapController, :create_client
+    post "/bootstrap/signing-keys", BootstrapController, :create_signing_key
+
+    post "/login/challenges", LoginController, :create_challenge
+    post "/login/challenges/consume", LoginController, :consume_challenge
+
+    get "/oauth/authorize", OAuthController, :authorize
+    post "/oauth/token", OAuthController, :token
+    get "/oauth/userinfo", OAuthController, :userinfo
+    post "/oauth/introspect", OAuthController, :introspect
+    post "/oauth/revoke", OAuthController, :revoke
+  end
 end
